cf7a92b0bce0c76afd84e5e48968228f2e482f39
ljm <ljm@addcn.com>
2020-05-27 11:41:33 +0800

    初始化仓库

---
+++ build/utils.js
@@ -0,0 +1,12 @@
+//处理文件路径
+
+const path =require('path');
+module.exports ={
+  resolve:function(dir){
+    return path.join(__dirname,'..',dir)
+  },
+  assetsPath:function(_path){
+    const assetsSubDirectory = "static";
+    return path.posix.join(assetsSubDirectory,_path);
+  }
+}
---
+++ build/webpack.config.base.js
@@ -0,0 +1,3 @@
+const HtmlWbpackPlugin = require('html-wbpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
