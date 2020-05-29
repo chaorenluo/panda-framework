38eb4a4108bb700d6fed963bc41bc3c6cc09a648
ljm <ljm@addcn.com>
2020-05-29 13:59:43 +0800

    配置环境

--- packages/nButtons/index.js
+++
@@ -1,7 +0,0 @@
-import nButtons from './src/index.vue';
-
-nButtons.install = function (Vue) {
-  Vue.component(nButtons.name, nButtons);
-};
-
-export default nButtons;
--- packages/nButtonsd/index.js
+++
@@ -1,7 +0,0 @@
-import nButtonsd from './src/index.vue';
-
-nButtonsd.install = function (Vue) {
-  Vue.component(nButtonsd.name, nButtonsd);
-};
-
-export default nButtonsd;
